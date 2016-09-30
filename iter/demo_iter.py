c98dc8eab5cb08c292425f2843828e5ffcb41a38
billychou <zsc1528@gmail.com>
2016-09-30 08:33:17 +0800

    列表

--- iter/demo_iter.py
+++ iter/demo_iter.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
+from __future__ import division
+import random
 import itertools
 
+#ita
 def range(start, end=None, inc=1.0):
     if end is None:
         end = start + 0.0
@@ -12,6 +15,12 @@ def range(start, end=None, inc=1.0):
             break
         yield next
 
+score = [random.randint(0, 100) for i in range(40)]
+print score
+num = len(score)
+ave_num = sum(score)/len(score)
+less_num = len([i for i in score if i < ave_num])
+print less_num
 
 if __name__=="__main__":
     for x in range(3, 6):
