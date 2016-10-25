f3a5fe863d65d7b0a4a7e7a0a928afd4094e6915
songchuan.zhou <songchuan.zhou@alibaba-inc.com>
2016-10-26 07:48:33 +0800

    python设计模式

---
+++ patterns/factory_method.py
@@ -0,0 +1,65 @@
+import json
+import xml.etree.ElementTree as etree
+
+
+class JSONConnector(object):
+    def __init__(self, filepath):
+        self.data = dict()
+        with open(filepath, mode='r', encoding='utf-8') as f:
+            self.data = json.load(f)
+
+    @property
+    def parsed_data(self):
+        return self.data
+
+
+class XMLConnector(object):
+    def __init__(self, filepath):
+        self.tree = etree.parse(filepath)
+
+    @property
+    def parsed_data(self):
+        return self.tree
+
+
+def connection_factory(filepath):
+    if filepath.endswith('json'):
+        connector = JSONConnector
+    elif filepath.endswith('xml'):
+        connector = XMLConnector
+    else:
+        rasie ValueError("Cannot connect to {}".format(filepath))
+    return connector(filepath)
+
+def connect_to(filepath):
+    factory = None
+    try:
+        factory = connection_factory(filepath)
+    except ValueError as ve:
+        print(ve)
+    return factory
+
+def main():
+    sqlite_factory = connect_to('data/person.sq3')
+    print()
+    xml_factory = connect_to('data/person.xml')
+    xml_data = xml_factory.parsed_data
+    liars = xml_data.findall(".//{}[{}='{}']".format('person', 'lastName', 'Liar'))
+    print('found: {} persons'.format(len(liars)))
+    for liar in liars:
+        print('first name: {}'.format(liar.find('firstName').text))
+        print('last name: {}'.format(liar.find('lastName').text))
+        [print('phone number ({})'.format(p.attrib['type']), p.text) for p in liar.find('phoneNumbers')]
+    print()
+
+    json_factory = connect_to('data/default.json')
+    json_data = json_factory.parsed_data
+    print('found: {} donuts'.format(len(json_data)))
+    for dount in json_data:
+        print('name: {}'.format(dount['name']))
+        print('price: ${}'.format(dount['ppu']))
+        [print('topping: {} {}'.format(t['id'], t['type'])) for t in dount['topping']]
+
+
+if __name__ == '__main__':
+    main()
