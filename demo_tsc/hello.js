c20cd623751c31df6b9ae96c9dfa28cb644b1715
billychou <zsc1528@gmail.com>
2017-01-18 17:40:08 +0800

    angularjs hell

--- demo_tsc/hello.js
+++ demo_tsc/hello.js
@@ -5,7 +5,9 @@ alert("hello world in TypeScript");
 function Add(left, right) {
     return left + right;
 }
-function area(shape, width, height) {
-    var area = width * height;
-    return "I'm a " + shape + " with an area of " + area + "cm squared.";
+function area(shape) {
+    var area = shape.width * shape.height;
+    return "I'm a " + shape.name + " with an area of " + area + "cm squared.";
 }
+console.log(area({ name: "rectangle", width: 30, height: 15 }));
+console.log(area({ name: "square", width: 40, height: 50 }));
--- demo_tsc/hello.ts
+++ demo_tsc/hello.ts
@@ -8,7 +8,45 @@ function Add(left: number, right: number): number {
     return left + right;
 }
 
-function area(shape: string, width: number, height: number) {
-    var area = width * height;
-    return "I'm a " + shape + " with an area of " + area + "cm squared.";
+//接口类型批注
+interface Shape {
+    name: string;
+    width: number;
+    height: number;
+    color?: string;
 }
+
+function area(shape: Shape) {
+    var area = shape.width * shape.height;
+    return "I'm a " + shape.name + " with an area of " + area + "cm squared.";
+}
+
+//function 更换成 ()=>
+var shape1 = {
+    name: "rectangle",
+    popup: function(){
+        console.log('This inside popup():', this.name);
+        setTimeout(function(){
+            console.log("This inside setTimeout():", this.name);
+            console.log("I'm a " + this.name + "!");
+        }, 3000);
+    }
+};
+
+
+
+class Shape2 {
+    area: number;
+    color: string;
+    constructor (name: string, width: number, height: number) {
+        this.area = width * height;
+        this.color = "pink";
+    };
+
+    shoutout() {
+        return "I'm " + this.color + " " + this.name + " with an area of " + this.area + "cm squared.";
+    }
+}
+
+console.log( area({name: "rectangle", width: 30, height: 15}))
+console.log( area( {name: "square", width: 40, height: 50}))
