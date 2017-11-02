a1811ff0beeaf51e9e104eb264b655049fdc534a
billychou <zsc1528@gmail.com>
2017-11-02 21:47:00 +0800

    the shelve module usage method

--- cookbook/dump_db_pickle.py
+++ cookbook/dump_db_pickle.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python
 #-*-coding:utf-8-*-
 #@Time: 17/10/31下午11:45
-#@Author: songchuan.zhou@alibaba-inc.com
+#@Author: songchuan.zhou@alibaba-inc.
+
+import pickle
+dbfile = open("people-pickle", 'rb')  #使用python3
+
+db = pickle.load(dbfile)
+for key in db:
+    print(key, "=> \n", db[key])
+print(db["sue"]["name"])
